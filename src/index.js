cfb74b8600b90822e3c5d678afaadab2db514fb4
Pedro Alexandre Pacheco <pedro.pacheco.a@gmail.com>
2020-07-30 21:08:43 -0300

    unindo esforçoes de funçoes

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const urlpai = 'https://www.gov.br/pt-br/noticias/ultimas-noticias?b_start:int=0';
+
+function extraidados(link) {
+    axios.get(link)
+        .then(resp => {
+            const dadoshtml = resp.data;
+            const $ = cheerio.load(dadoshtml);
+            const titulo = $('h1').text();
+            const linkimg = $('img').attr('src');
+            const datapublicacao = $('span[class="value"]').text();
+            const texto = $('div[property="rnews:articleBody"]').text();
+
+            const dados = { titulo, linkimg, datapublicacao, texto }
+
+            console.log(dados);
+        });
+};
+
+const links = axios.get(urlpai)
+    .then(resp => {
+        const dadoshtml = resp.data;
+        const $ = cheerio.load(dadoshtml);
+        const dados = []
+        $('a[class="summary url"]').each((i, e) => {
+            const link = $(e).attr('href');
+            //console.log(link)
+            dados.push(link)
+        });
+        //console.log(dados);
+        return dados
+    });
+
+    async function main(){
+        const lnks = await links;
+        lnks.map((i,e)=>{
+            extraidados(i);
+        })
+    };
+
+    main();
